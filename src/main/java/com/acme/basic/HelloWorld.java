bf736935ad60989a2a182d72c9f3d960a5f7ba34
Satya <119496526+F733591@users.noreply.github.com>
2023-02-08 12:10:16 +0000

    addressing coverage issues

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -1,19 +1,13 @@
 package com.acme.basic;
 
 public class HelloWorld {
-  
-  int i=1;
-  int j=2;
 
   void sayHello() {
     System.out.println("Hello World!");
-    System.out.println("addition:"+add(i,j));
   }
 
   void notCovered() {
     System.out.println("This method is not covered by unit tests");
   }
-  int add(int i, int j){
-    return i+j;
-  }
+
 }
